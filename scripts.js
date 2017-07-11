289ea232547563dd8f5c51aac1b02ee9b448ff25
Jerry Hoglen <jerryhoglen@me.com>
2017-07-10 22:49:00 -0400

    Created user log in / log out and added restrictions to site access with jquery

--- scripts.js
+++ scripts.js
@@ -1,5 +1,7 @@
 $(function() {
 
+    $('#hide-nav').hide();
+
     // Initialize Firebase
     var config = {
         apiKey: "AIzaSyC_WglPteiSNxTekpJ0LW9s7drZEeoqt7Y",
@@ -11,21 +13,65 @@ $(function() {
     };
 
     firebase.initializeApp(config);
-    firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        // ...
-    });
-    firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        // ...
+
+    // Get elements
+    var txtEmail = $('#txtEmail');
+    var txtPassword = $('#txtPassword');
+    var btnLogin = $('#btnLogin');
+    var btnSignUp = $('#btnSignUp');
+    var btnLogout = $('#btnLogout');
+
+    btnLogin.on('click', function() {
+        event.preventDefault();
+        var email = txtEmail.val();
+        var password = txtPassword.val();
+        var auth = firebase.auth();
+
+        firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            $('#btnSignUp').hide();
+            $('#hide-nav').show();
+        });
     });
 
-    firebase.auth().signOut()
-        .catch(function(err) {
-            // Handle errors
+    btnSignUp.on('click', function() {
+        event.preventDefault();
+        var email = txtEmail.val();
+        var password = txtPassword.val();
+        var auth = firebase.auth();
+
+
+        firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            $('#btnSignUp').hide();
+            $('#hide-nav').show();
         });
+    });
+
+    btnLogout.on('click', function() {
+        firebase.auth().signOut().then(function() {
+            alert("You have been logged out!");
+            $('#hide-nav').hide();
+        }).catch(function(error) {
+            // An error happened.
+        });
+    });
 })
+
+function initMap() {
+    var myLocation = { lat: 40.73978, lng: -73.98983 };
+    console.log(myLocation);
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 4,
+        center: myLocation,
+    });
+    var marker = new google.maps.Marker({
+        position: myLocation,
+        map: map
+    });
+    console.log("this works");
+}
