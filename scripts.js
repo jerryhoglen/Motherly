d7f1b81443e2383340563c199fb8964c131bca2d
Jerry Hoglen <jerryhoglen@me.com>
2017-07-12 22:39:40 -0400

    Built out reply functionality on ask page

--- scripts.js
+++ scripts.js
@@ -97,25 +97,29 @@ $(function() {
             var question = post.val().question;
 
             var newElement = '<div class="panel panel-default">' +
-                '<div class="panel-heading">' + '<h4>' + headline + '</h4>' + '<br>' + '<p>' + question + '</p>' + '</div>';
-            var newReply = '<button id="reply-btn" type="button" class="btn btn-primary" data-toggle="modal" data-target="#reply-modal">Reply to question</button>' + '<hr>';
+                '<div class="panel-heading">' + '<h3 class="new-heading">' + headline + '</h3>' + '<br>' + '<p>' + question + '</p>' + '</div>';
+            var newReply = '<button id="reply-btn" type="button" class="btn btn-default" data-toggle="modal" data-target="#reply-modal">Reply to question</button>';
             $('#new-posts').append(newElement);
             $('#new-posts').append(newReply);
         });
     });
-    $('#reply-btn').on('click', function() {
-        console.log('works');
+
+    $('#save-btn').on('click', function(e) {
+        e.preventDefault();
+        
+
+        var questionReply = $('#answer-question').val();
+        var userReply = $('#username-reply').val();
+        console.log(questionReply);
+        var newElementTwo = '<div class="panel panel-default">' +
+            '<div class="panel-body">' + '<h3>' + 'User Reply' + '<hr>' + '</h3>' + '<h4>' + userReply + '</h4>' + '<p>' + questionReply + '</p>' + '</div>';
+        $('#new-reply').append(newElementTwo);
+        $('#reply-btn').hide();
+
+
     });
-})
 
-
-
-
-
-
-
-
-
+});
 
 
 
@@ -126,16 +130,69 @@ $(function() {
                   Google maps api
 
 ****************************************************/
-function initMap() {
-    var myLocation = { lat: 40.73978, lng: -73.98983 };
-    console.log(myLocation);
+function initAutocomplete() {
     var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 15,
-        center: myLocation,
+        center: { lat: 40.712784, lng: -74.005941 },
+        zoom: 13,
+        mapTypeId: 'roadmap'
     });
-    var marker = new google.maps.Marker({
-        position: myLocation,
-        map: map
+
+    // Create the search box and link it to the UI element.
+    var input = document.getElementById('pac-input');
+    var searchBox = new google.maps.places.SearchBox(input);
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+    // Bias the SearchBox results towards current map's viewport.
+    map.addListener('bounds_changed', function() {
+        searchBox.setBounds(map.getBounds());
     });
-    console.log("this works");
-}
+
+    var markers = [];
+    // Listen for the event fired when the user selects a prediction and retrieve
+    // more details for that place.
+    searchBox.addListener('places_changed', function() {
+        var places = searchBox.getPlaces();
+
+        if (places.length == 0) {
+            return;
+        }
+
+        /***   // Clear out the old markers.
+           markers.forEach(function(marker) {
+             marker.setMap(null);
+           });
+           markers = []; ****/
+
+        // For each place, get the icon, name and location.
+        var bounds = new google.maps.LatLngBounds();
+        places.forEach(function(place) {
+            if (!place.geometry) {
+                console.log("Returned place contains no geometry");
+                return;
+            }
+            var icon = {
+                url: place.icon,
+                size: new google.maps.Size(71, 71),
+                origin: new google.maps.Point(0, 0),
+                anchor: new google.maps.Point(17, 34),
+                scaledSize: new google.maps.Size(25, 25)
+            };
+
+            // Create a marker for each place.
+            markers.push(new google.maps.Marker({
+                map: map,
+                icon: icon,
+                title: place.name,
+                position: place.geometry.location
+            }));
+
+            if (place.geometry.viewport) {
+                // Only geocodes have viewport.
+                bounds.union(place.geometry.viewport);
+            } else {
+                bounds.extend(place.geometry.location);
+            }
+        });
+        map.fitBounds(bounds);
+    });
+};
